85cd0d333e5a80292182b174deaffae0e122bc40
Yaroslav Nikolaiko <ynikolaiko@intropro.com>
2016-11-24 21:13:19 +0200

    remove spring could dependencies

--- src/main/java/demo/FoodReviewsApplication.java
+++ src/main/java/demo/FoodReviewsApplication.java
@@ -2,10 +2,8 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class FoodReviewsApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodReviewsApplication.class, args);
